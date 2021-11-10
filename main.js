aa8bd1ad76b1cbf02ca075ad1b20e08dd13813f4
Halil Özat <halilozat@hotmail.com>
2021-11-10 22:25:28 +0300

    Commit without error

---
+++ main.js
@@ -0,0 +1,81 @@
+const electron = require("electron")
+
+//url ve path sayfalarımızı oluştururken hangi html sayfasını 
+//kullanacağımızı belirttiğimiz yöntemde kullandığımız araçlar
+const url = require("url")
+const path = require("path")
+
+
+const { app, BrowserWindow, Menu } = electron;
+
+//ana sayfa
+let mainWindow, mainMenu, mainMenuTemplate;
+
+//uygulamamız hazır olduğunda bu çalışacak! (yani başlatırken)
+app.on('ready', () => {
+    console.log("running!")
+
+    console.log(process.platform)
+
+    mainWindow = new BrowserWindow({})
+    mainWindow.loadURL(
+        url.format({
+            pathname: path.join(__dirname, "main.html"),
+            //nereden geldiğini belirtir, dosya tipini
+            protocol: "file",
+            //file://abc
+            slashes: true
+        })
+    )
+
+    mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
+
+    //bu menüyü benim uygulamamın menüsü olarak ayarla
+    Menu.setApplicationMenu(mainMenu)
+})
+
+mainMenuTemplate = [
+    {
+        label: "Dosya",
+        //altmenu
+        submenu: [
+            {
+                label: "Yeni Todo Ekle"
+            },
+            {
+                label: "Tümünü Sil"
+            },
+            {
+                label: "Çıkış",
+                //kısa yol tuşu atama
+                accelerator: process.path == "darwin" ? "Command+Q" : "Ctrl:Q",
+                role: "quit"
+            }
+        ]
+    },
+    //Geliştirici konsol ve yenile için yeni menu
+    {
+        label: "Dev Tools",
+        submenu: [
+            {
+                label: "Konsolu Aç",
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                label: "Yenile",
+                role: "reload"
+            }
+        ]
+
+    }
+]
+
+if (process.platform == "darwin") {
+    mainMenuTemplate.unshift({
+        label: app.getName(),
+        role: "TODO"
+    })
+}
+
